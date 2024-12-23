56492a1b609044d04f42123f7d7c16a78a7b9831
erikckartman <severyukov05d@gmail.com>
2024-12-23 14:00:02 +0100

    Stan Gets Damage

--- Assets/Scripts/Combat/StanCombat.cs
+++ Assets/Scripts/Combat/StanCombat.cs
@@ -14,15 +14,21 @@ public class StanCombat : NetworkBehaviour
     [SerializeField] private Slider powerbar;
     private int health = 100;
     private int power = 0;
+
+    private float forwardForce = 15f;
+    private float upwardForce = 5f;
+    private float dashDuration = 0.2f;
     private bool isAttacking = false;
 
     [SerializeField] private ParticleSystem vomit;
     private int vomitDamage = 75;
+    private List<Collider> damagedEnemies = new List<Collider>();
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0) && Object.HasInputAuthority)
         {
+            StartCoroutine(Dash());
             Punch();
         }
 
@@ -30,6 +36,8 @@ public class StanCombat : NetworkBehaviour
         {
             Special();
         }
+
+        GetDamage(15);
     }
 
     private void Punch()
@@ -57,11 +65,50 @@ public class StanCombat : NetworkBehaviour
         }        
     }
 
+    private void GetDamage(int damage)
+    {
+        if (Object.HasInputAuthority && !isAttacking)
+        {
+            Collider[] hitEnemies = Physics.OverlapSphere(transform.position, 1f, enemyLayer);
+
+            if (hitEnemies.Length > 0)
+            {
+                foreach (Collider enemy in hitEnemies)
+                {
+                    if (!damagedEnemies.Contains(enemy))
+                    {
+                        damagedEnemies.Add(enemy);
+
+                        Vector3 forceDirection = transform.position - enemy.transform.position;
+                        var rb = GetComponent<Rigidbody>();                    
+                        forceDirection.y = 0f;
+                        rb.AddForce(forceDirection.normalized * 5f, ForceMode.Impulse);
+                        
+                        health -= damage;
+                        healthbar.value = health;
+
+                        if (health <= 0)
+                        {
+                            Die();
+                        }
+                    }
+                }
+                StartCoroutine(ClearDamagedEnemies());
+            }
+        }
+    }
+
+    private IEnumerator ClearDamagedEnemies()
+    {
+        yield return new WaitForSeconds(1.0f);
+        damagedEnemies.Clear(); 
+    }
+    
     [Rpc(RpcSources.InputAuthority, RpcTargets.All)]
     private void InflictDamageRpc(NetworkId enemyId, int damage)
     {
         var enemy = Runner.TryFindObject(enemyId, out var networkObject) ? networkObject : null;
-
+        
         if (enemy != null)
         {
             var healthComponent = enemy.GetComponent<Enemy>();
@@ -79,7 +126,7 @@ public class StanCombat : NetworkBehaviour
                 }                
                 powerbar.value = power;
 
-                isAttacking = false;
+                
                 Debug.Log($"Inflicted {damage} damage to {enemyId}");
             }
             else
@@ -102,6 +149,22 @@ public class StanCombat : NetworkBehaviour
         }
     }
 
+    private IEnumerator Dash()
+    {
+        isAttacking = true;
+        float startTime = Time.time;
+        while (Time.time < startTime + dashDuration)
+        {
+            transform.Translate(Vector3.forward * forwardForce * Time.deltaTime);
+            transform.Translate(Vector3.up * upwardForce * Time.deltaTime);
+            yield return null;
+        }
+        if(Time.time >= startTime + dashDuration)
+        {
+            isAttacking = false;
+        }
+    }
+
     private void Special()
     {
         Debug.Log("Special");
@@ -120,24 +183,6 @@ public class StanCombat : NetworkBehaviour
                     InflictDamageRpc(networkObject.Id, vomitDamage);
                 }
             }
-        }
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.layer == enemyLayer && !isAttacking)
-        {
-            TakeDamage(15);
-        }
-    }
-
-    private void TakeDamage(int damage)
-    {
-        health -= damage;
-        healthbar.value = health;
-        if (health <= 0)
-        {
-            Die();
         }
     }
 
